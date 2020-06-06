77f7e09cb8228c6d276e39f28c9725a27ea7e67c
omiinaya <omiinaya@gmail.com>
2020-06-06 02:00:29 -0400

    this is literally user directory but done.

--- client/src/index.js
+++ client/src/index.js
@@ -21,24 +21,22 @@ export function find() {
             console.log("number of books: " + count);
             console.log(results)
             newBookRow()
-            //updateRowData()
             return response.data;
         })
 }
 
 export function newBookRow() {
     for (var i = 0; i < results.length; i++) {
+        
         const div = document.createElement('div');
         div.innerHTML = `
         <div id="book-`+i+`
             <p id="title-`+i+`">Title: `+results[i].volumeInfo.title+`</p>
             <p id="author-`+i+`">Author: `+results[i].volumeInfo.authors[0]+`</p>
+            <p id="description-`+i+`">Description: `+results[i].volumeInfo.description+`</p>
+            <img id="image-`+i+`" src="`+results[i].volumeInfo.imageLinks.smallThumbnail+`">
         </div>
         `;
         document.getElementById('content').appendChild(div);
     }
-}
-
-export function updateRowData() {
-    document.getElementById("title-").innerText = results[0].volumeInfo.title;
 }
